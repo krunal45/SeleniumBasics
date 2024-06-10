fbf3d3f256b95faa22b9a11fe9fb9ff5f96a3949
krunal45 <47133975+krunal45@users.noreply.github.com>
2024-06-10 11:04:05 +0530

    TestNG Practice

---
+++ src/test/java/CreateContactTest.java
@@ -0,0 +1,39 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+    @BeforeTest
+    void logintoApplication(){
+        System.out.println("Successfully Logged In");
+    }
+
+    @BeforeTest
+    void openDBConnection(){
+        System.out.println("DB connection opened");
+    }
+
+    @Test(priority = 1,groups = {"Sanity"})
+    void verifyIfContactCreated(){
+        System.out.println("Contact Created in UI");
+    }
+
+    @Test(priority = 2,groups = {"Regression"})
+    void verifyIfContactSavedInDatabase(){
+        System.out.println("Contact saved in DB");
+    }
+
+    @AfterTest
+    void logoutOfApplication(){
+        System.out.println("Successfully Logged out");
+    }
+
+    @AfterTest
+    void closeDBConnection(){
+        System.out.println("DB connection closed");
+    }
+
+
+
+}
---
+++ src/test/java/UpdateContactTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class UpdateContactTest {
+
+    @Test(groups = {"Sanity"})
+    void updateContact(){
+        System.out.println("Contact Updated!");
+    }
+}
